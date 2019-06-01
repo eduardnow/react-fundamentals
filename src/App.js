03972303331f4b0971cfa199d9abf30b03e8dfbe
Eduard Castro <eduard.cas07@gmail.com>
2019-05-31 21:48:55 -0500

    Add struture basic - Author Quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './bootstrap.min.css'
+import React from "react";
+import "./App.css";
+import "./bootstrap.min.css";
 
-function App() {
+function Hero() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book writtem by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn" style={{ backgroundColor: "white" }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="author-image" alt="Author" />
+      </div>
+
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return <div />;
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="https://www.wikimedia.org/">Wiki</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Demo
-        </a>
-      </header>
+      </div>
+    </div>
+  );
+}
+
+function App({ turnData }) {
+  return (
+    <div className="container">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
     </div>
   );
 }
