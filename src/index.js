e5b4899221d87bcbe33e87dbf116cdcae158f27c
Eduard Castro <eduard.cas07@gmail.com>
2019-06-05 13:11:37 -0500

    Add events to the quiz

--- src/index.js
+++ src/index.js
@@ -63,11 +63,21 @@ function getTurnData(authors) {
 }
 
 const state = {
-    turnData: getTurnData(authors)
+    turnData: getTurnData(authors),
+    highlight: 'none',
 };
 
-ReactDOM.render(<App {...state}/>, document.getElementById('root'));
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  render();
+}
 
+function render() {
+  ReactDOM.render(<App {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
